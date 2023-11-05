76ba0aea540641f316191b2bb1aee4d86951072d
David <daviddenoia72@gmail.com>
2023-11-05 14:37:46 +0000

    Añadida la clase DepthPageTransformer para probar animacion entre las paginas de introduccion

--- app/src/main/java/com/example/trabajofinalv2/MainActivity.kt
+++ app/src/main/java/com/example/trabajofinalv2/MainActivity.kt
@@ -1,16 +1,6 @@
 package com.example.trabajofinalv2
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.trabajofinalv2.ui.theme.TrabajoFinalV2Theme
 import androidx.appcompat.app.AppCompatActivity
 
 
