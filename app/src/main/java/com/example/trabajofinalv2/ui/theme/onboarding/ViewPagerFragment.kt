b613f4ce76b88ea644c3f63c922cab344efb8e5d
David <daviddenoia72@gmail.com>
2023-11-05 14:36:18 +0000

    Añadida la clase DepthPageTransformer para probar animacion entre las paginas de introduccion

--- app/src/main/java/com/example/trabajofinalv2/ui/theme/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/example/trabajofinalv2/ui/theme/onboarding/ViewPagerFragment.kt
@@ -39,9 +39,50 @@ class ViewPagerFragment : Fragment() {
 
         val viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
         viewPager.adapter = adapter
-
+        viewPager.setPageTransformer(DepthPageTransformer())
         return view
     }
 
 
+}
+
+class DepthPageTransformer : ViewPager2.PageTransformer {
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            when {
+                position < -1 -> {
+
+                    alpha = 0f
+                }
+                position <= 0 -> {
+
+                    alpha = 1f
+                    translationX = 0f
+                    scaleX = 1f
+                    scaleY = 1f
+                }
+                position <= 1 -> {
+
+                    alpha = 1 - position
+
+
+                    translationX = pageWidth * -position
+
+
+                    val scaleFactor = (MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position)))
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+                }
+                else -> { // (1,+Infinity]
+
+                    alpha = 0f
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val MIN_SCALE = 0.75f
+    }
 }
