5a9e680e4e97e2772ff76b23aaab5118b69c0bf9
anaMJimenezPerez <ana.jimenez121@alu.ulpgc.es>
2023-11-11 16:22:58 +0000

    desabilitar/habilitar botones y añadir dialog a restablecer contraseña

--- app/src/main/java/com/example/trabajofinalv2/RecuperarClave2.kt
+++ app/src/main/java/com/example/trabajofinalv2/RecuperarClave2.kt
@@ -1,10 +1,15 @@
 package com.example.trabajofinalv2
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -20,7 +25,25 @@ class RecuperarClave2 : Fragment() {
         val view = inflater.inflate(R.layout.fragment_restablecer_password2, container, false)
 
         val btnSend = view.findViewById<Button>(R.id.btnRestContra)
+        val inputCodigo = view.findViewById<EditText>(R.id.TextCodigo)
 
+        inputCodigo.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                btnSend.isEnabled = !s.isNullOrBlank()
+
+                if (btnSend.isEnabled) {
+                    btnSend.setBackgroundColor(Color.parseColor("#114B8D"))
+                } else {
+                    btnSend.setBackgroundColor(Color.parseColor("#416FA4"))
+                }
+            }
+        })
         btnSend.setOnClickListener {
 
             findNavController().navigate(R.id.action_pantallaClave2_to_pantallaClave3)
