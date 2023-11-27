03aa318ded1cbc18165179a7bc3235a88f4bde05
David <daviddenoia72@gmail.com>
2023-11-27 16:10:02 +0000

    Front del fragment de cambiar contraseña del menu desplegable

---
+++ app/src/main/java/com/example/trabajofinalv2/ui/theme/CambiarContraseña.kt
@@ -0,0 +1,23 @@
+package com.example.trabajofinalv2.ui.theme
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trabajofinalv2.R
+
+
+class CambiarContraseña : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.cambiar_contrasena, container, false)
+    }
+
+
+}
