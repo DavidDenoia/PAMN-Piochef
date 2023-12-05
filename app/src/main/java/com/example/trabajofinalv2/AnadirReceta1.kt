d881f9a7b659a2893c238ea657f812177f6dfa40
Marco <marco_nehuen@hotmail.com>
2023-12-05 21:55:50 +0000

    Push fots multiples

--- app/src/main/java/com/example/trabajofinalv2/AnadirReceta1.kt
+++ app/src/main/java/com/example/trabajofinalv2/AnadirReceta1.kt
@@ -9,27 +9,50 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.content.Intent
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.activity.result.contract.ActivityResultContracts.PickMultipleVisualMedia
 import androidx.navigation.fragment.findNavController
-
 
 class AnadirReceta1 : Fragment() {
 
     private var stepCount = 1
 
+    val pickMedia = registerForActivityResult(PickMultipleVisualMedia(3)) { uris ->
+        if(uris.isNotEmpty()){
+            val layoutIamgenes = view?.findViewById<LinearLayout>(R.id.layoutImagenes)
+            layoutIamgenes?.removeAllViews()
+            btnImage.setImageURI(uris.get(0))
+            for (uri in uris) {
+                val newbtnImage = ImageButton(context)
+                newbtnImage.layoutParams = btnImage.layoutParams
+                newbtnImage.setImageURI(uri)
+                layoutIamgenes?.addView(newbtnImage)
+                Log.i(tag,"creado")
+            }
+
+        }else{
+
+        }
+    }
+    lateinit var btnImage: ImageButton
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_anadir_receta1, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val nextPageButton = view.findViewById<ImageButton>(R.id.nextPageButton)
-
         val stepsLayout = view.findViewById<LinearLayout>(R.id.stepsLayout)
+        btnImage = view.findViewById(R.id.recipeImage)
         val addStepButton = view.findViewById<ImageButton>(R.id.addStepButton)
 
         addStepButton.setOnClickListener {
@@ -47,6 +70,9 @@ class AnadirReceta1 : Fragment() {
             stepsLayout.addView(newStep)
         }
 
+        btnImage.setOnClickListener{
+            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        }
         nextPageButton.setOnClickListener {
             findNavController().navigate(R.id.action_pantallaAnadirReceta_to_pantallaAnadirReceta2)
         }
--- app/src/main/java/com/example/trabajofinalv2/AnadirReceta2.kt
+++ app/src/main/java/com/example/trabajofinalv2/AnadirReceta2.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import androidx.core.view.allViews
 import androidx.core.view.marginLeft
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -18,7 +19,7 @@ class AnadirReceta2 : Fragment() {
     private var stepCount = 1
     //Crea un firebaseAuth object
     lateinit var auth: FirebaseAuth
-
+    private val editTextsList = mutableListOf<EditText>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -80,6 +81,9 @@ class AnadirReceta2 : Fragment() {
             uni.hint = "ml"
             uni.inputType = android.text.InputType.TYPE_CLASS_TEXT
 
+            editTextsList.add(nom)
+            editTextsList.add(can)
+            editTextsList.add(uni)
             // Agrega el nuevo EditText al LinearLayout de los pasos
             layout.addView(nom)
             layout.addView(can)
@@ -96,15 +100,32 @@ class AnadirReceta2 : Fragment() {
         //se autentifica
         auth = FirebaseAuth.getInstance()
         //funcion pilla todos los datos
-        recogeDatos()
+        val datosinicial = recogeDatos()
+        val ingredientes = obtenerTextosDePasos()
         //los mete en database
 
         //una vez metidos navega al siguiente
         findNavController().navigate(R.id.action_pantallaAnadirReceta2_to_pantallaAnadirReceta3)
     }
 
-    fun recogeDatos(){
+    fun recogeDatos(): List<String>{
         var datos = Vector<String>();
         //pilla todos los datos de la pantalla
+        val prepT = view?.findViewById<EditText>(R.id.tiempoInput)
+        val raciones = view?.findViewById<EditText>(R.id.racionesInput)
+
+        datos.add(prepT?.text.toString())
+        datos.add(raciones?.text.toString())
+        return datos
+    }
+    fun obtenerTextosDePasos(): List<String> {
+        val textosDePasos = mutableListOf<String>()
+
+        for (editText in editTextsList) {
+            val texto = editText.text.toString()
+            textosDePasos.add(texto)
+        }
+
+        return textosDePasos
     }
 }
