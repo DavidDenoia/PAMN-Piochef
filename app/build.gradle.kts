abc89a68818ad19db18d82c56c049b5c607577d8
David <daviddenoia72@gmail.com>
2023-12-07 23:30:39 +0000

    Primera aproximacion para ver recetas. Los botones de preparacion e ingredientes cambian de color y de framelayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation( "androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.google.android.material:material:1.10.0")
 }
--- app/src/main/java/com/example/trabajofinalv2/verRecetas.kt
+++ app/src/main/java/com/example/trabajofinalv2/verRecetas.kt
@@ -6,9 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import org.checkerframework.common.subtyping.qual.Bottom
 
 class verRecetas : Fragment() {
+    private var botonPreparacion: Button? = null
+    private var botonIngredientes: Button? = null
+    private var contenedorPreparacion: FrameLayout? = null
+    private var contenedorIngredientes: FrameLayout? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +28,106 @@ class verRecetas : Fragment() {
         val recipeName = arguments?.getString("recipeName")
         Log.e("RecipeClick", "Pepito: $recipeName, Usuario: $user")
         // Inflate the layout for this fragment
+
+
+
+
+        /*botonPreparacion = view?.findViewById<Button>(R.id.botonPreparacion)
+        botonIngredientes = view?.findViewById<Button>(R.id.botonIngredientes)
+        contenedorPreparacion = view?.findViewById<FrameLayout>(R.id.contenedorPreparacion)
+        contenedorIngredientes = view?.findViewById<FrameLayout>(R.id.contenedorIngredientes)*/
+
+        /*mostrarPreparacion()
+        botonPreparacion?.setOnClickListener {
+            mostrarPreparacion()
+            botonPreparacion?.apply {
+                setBackgroundColor(resources.getColor(R.color.custom_color_primary))
+                setTextColor(resources.getColor(R.color.white))
+            }
+            botonIngredientes?.apply {
+                setBackgroundColor(resources.getColor(R.color.white))
+                setTextColor(resources.getColor(R.color.black))
+            }
+        }
+        botonIngredientes?.setOnClickListener {
+            mostrarIngredientes()
+            botonIngredientes?.apply {
+                setBackgroundColor(resources.getColor(R.color.custom_color_primary))
+                setTextColor(resources.getColor(R.color.white))
+            }
+            botonPreparacion?.apply {
+                setBackgroundColor(resources.getColor(R.color.white))
+                setTextColor(resources.getColor(R.color.black))
+            }
+        }*/
+
+        /*botonPreparacion?.setOnClickListener {
+            mostrarPreparacion()
+            botonPreparacion?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.custom_color_primary))
+            botonPreparacion?.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonIngredientes?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonIngredientes?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        }
+
+        botonIngredientes?.setOnClickListener {
+            mostrarIngredientes()
+            botonIngredientes?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.custom_color_primary))
+            botonIngredientes?.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonPreparacion?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonPreparacion?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        }
+
+        // Muestra la preparación por defecto
+        mostrarPreparacion()*/
+
+
+
         return inflater.inflate(R.layout.fragment_verrecetas, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        botonPreparacion = view?.findViewById<Button>(R.id.botonPreparacion)
+        botonIngredientes = view?.findViewById<Button>(R.id.botonIngredientes)
+        contenedorPreparacion = view?.findViewById<FrameLayout>(R.id.contenedorPreparacion)
+        contenedorIngredientes = view?.findViewById<FrameLayout>(R.id.contenedorIngredientes)
+
+        botonPreparacion?.setOnClickListener {
+            mostrarPreparacion()
+            botonPreparacion?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.custom_color_primary))
+            botonPreparacion?.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonIngredientes?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonIngredientes?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        }
+
+        botonIngredientes?.setOnClickListener {
+            mostrarIngredientes()
+            botonIngredientes?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.custom_color_primary))
+            botonIngredientes?.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonPreparacion?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+            botonPreparacion?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        }
+
+        mostrarPreparacion()
+
+    }
+
+    private fun mostrarPreparacion(){
+        contenedorPreparacion?.visibility = View.VISIBLE
+        contenedorIngredientes?.visibility = View.GONE
+        Log.d("MostrarPreparacion", "Mostrando la preparación de la receta")
+    }
+
+
+
+    private fun mostrarIngredientes() {
+
+        contenedorPreparacion?.visibility = View.GONE
+        contenedorIngredientes?.visibility = View.VISIBLE
+        Log.d("Mostrar ingredientes", "Mostrando los ingredientes de la receta")
     }
 
 }
